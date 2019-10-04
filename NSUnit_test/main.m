7b460d349974e7c12f003239379774ee0c9ba45d
Gregory John Casamento <greg.casamento@gmail.com>
2019-10-04 09:58:19 -0400

    Figuring out now NSUnit works.

--- NSUnit_test/main.m
+++ NSUnit_test/main.m
@@ -8,10 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
+/* Show whether test was passed */
+void passTest(BOOL flag,
+              NSString *msg)
+{
+    if(flag == YES)
+    {
+        NSLog(@"PASS: %@",msg);
+    }
+    else
+    {
+        NSLog(@"FAIL: %@",msg);
+    }
+}
+
+/* Run all tests */
+void runTests()
+{
+    NSUInteger abstractValue = 1.0;
+    NSMeasurement *seconds = [[NSMeasurement alloc] initWithDoubleValue:abstractValue unit:[NSUnitDuration seconds]];
+    NSMeasurement *minutes = [[NSMeasurement alloc] initWithDoubleValue:abstractValue unit:[NSUnitDuration minutes]];
+    // 61.0 seconds, measured in the dimension's base unit
+    double totalTime = [seconds doubleValue] + [minutes doubleValue];
+    passTest((totalTime == 61.0), @"Adds up to 61");
+    
+    NSUnitAcceleration *ua = [[NSUnitAcceleration alloc] initWithSymbol:@"m/s2"];
+    NSUnitConverter *c = [[ua class] baseUnit];
+    NSLog(@"ua = %@", ua);
+}
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    @autoreleasepool
+    {
+        runTests();
     }
     return 0;
 }
