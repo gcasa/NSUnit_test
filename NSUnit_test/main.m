bcbed985d06c8466afa02fc879d12d53a458a5ad
Gregory John Casamento <greg.casamento@gmail.com>
2019-10-04 02:30:06 -0400

    Initial Commit

---
+++ NSUnit_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSUnit_test
+//
+//  Created by Gregory John Casamento on 10/4/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
